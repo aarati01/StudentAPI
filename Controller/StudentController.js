c247847a4da622d6f8b7471a1b70d29e103722be
aarati01 <131001442+aarati01@users.noreply.github.com>
2024-11-18 20:57:42 -0500

    main file app.js is added to give a route

--- Controller/StudentController.js
+++ Controller/StudentController.js
@@ -1,3 +1,9 @@
 var mongoose = require("mongoose");
 const StudentModel = require("../model/StudentsModel");
-
+module.exports = {
+  getallStudents: async function (req, res) {
+    console.log("we are inside the all func body");
+    const result = await StudentModel.find();
+    res.render("Student.ejs", { alltheStudent: result });
+  },
+};
